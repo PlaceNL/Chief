ea670e26eb8da79200e4da49fdc8a4b69d33dd1c
Kaan Mollaoglu <kaanmol@outlook.com>
2023-04-01 01:49:28 +0200

    Added datefield to json stats for Grafana

--- src/ws/util/statsUtil.js
+++ src/ws/util/statsUtil.js
@@ -3,6 +3,7 @@ import {ALLOWED_CAPABILITIES} from '../handler/capabilities.js';
 export function gatherStats(chief, complex) {
     const stats = {
         ...chief.stats,
+        date: new Date(),
         activeConnections: chief.clients.size
     };
 
